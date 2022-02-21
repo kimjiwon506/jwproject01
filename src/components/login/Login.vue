bf1710e9db8899feb441d1096e44e91c2b742dfb
kimjiwon506 <kimjiwon506@gmail.com>
2022-02-21 21:10:23 +0900

    join,login테스트

---
+++ src/components/login/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <h1>{{ msg }}</h1>
+    <div>
+        <p>test</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'login 페이지 입니다'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scope>
+    .login {
+        background: #cccccc;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Test from '@/components/Test'
+import Join from '@/components/join/Join'
+import Login from '@/components/login/Login'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Test
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/join',
+      name: 'Join',
+      compotent: Join
     }
   ]
 })

c6f83a638f0917269d2fcd296eea692d7defe2d2
kimjiwon506 <kimjiwon506@gmail.com>
2022-02-27 13:33:15 +0900

    Revert "fix:라우터 추가 , 사스 오류나는것 버전해결"
    This reverts commit c9165f3113c3bbe85831c0572ae3eb66e6bfbaf8.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Test from '@/components/Test'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Test
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })

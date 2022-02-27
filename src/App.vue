d9b893238e1135fa3619f6a19c8b74bf7e2e70b9
limenough <limenough@gmail.com>
2022-02-27 15:52:26 +0900

    프로젝트 초기화 후 기본 환경셋팅 - scss, vuew, router, font-awesome, getters

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
-<!-- div id="wrap" :class{'isTabBar':$route.meta.tabBar} -->
-  <div id="app">
-    <router-view/>
+  <div id="app" class="note">
+    <transition name="page">
+        <Test></Test>
+      <router-view></router-view>
+    </transition>
   </div>
-<!-- /div id="wrap" :class{'isTabBar':$route.meta.tabBar} -->
 </template>
 
 <script>
+import Test from './views/Test.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Test,
+  },
 }
 </script>
 
 <style lang="scss">
-@import '@/assets/styles/_variables.scss';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $bgColor;
-  margin-top: 60px;
-}
+  .page-enter-active, .page-leave-active { 
+    opacity: 1;
+    transition: opacity 0.5s;
+  }
+ 
+  .page-enter, .page-leave-to { 
+    opacity: 0;
+  }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+// vue 구성/설정 파일
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; //Bundle 분석 플러그인
+
+module.exports = {
+    lintOnSave: false,
+    // configureWebpack: {
+    //   plugins: [new BundleAnalyzerPlugin()],
+    //   resolve: {
+    //     alias: {
+    //       moment: 'moment/src/moment' //moment 용량 줄이기
+    //     }
+    //   }
+    // },
+    css: {
+      loaderOptions: { //vue-loader 의 내부 구성을 변경하는데 사용할 수 있는 css 옵션
+        sass: {
+          data: `@import "@/assets/scss/app.scss";`
+        },
+      }
+  }
+}

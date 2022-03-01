a60168b50f98731711ec9838f1151dcf75ae0f66
limenough <limenough@gmail.com>
2022-03-01 16:47:25 +0900

    rem 설정 코드 주석 > 추후 모바일뷰 고려시 활성화 예정

--- src/main.js
+++ src/main.js
@@ -20,10 +20,10 @@ new Vue({
 }).$mount('#app')
 
 /* rem 적용 */
-const htmlDoc = document.documentElement;
-htmlDoc.style.fontSize =  parseInt(htmlDoc.offsetWidth / 360 * 100) + '%';
+// const htmlDoc = document.documentElement;
+// htmlDoc.style.fontSize =  parseInt(htmlDoc.offsetWidth / 360 * 100) + '%';
 
-const metaTag = document.createElement('meta');
-metaTag.name = "viewport";
-metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no";
-document.getElementsByTagName('head')[0].appendChild(metaTag);
+// const metaTag = document.createElement('meta');
+// metaTag.name = "viewport";
+// metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no";
+// document.getElementsByTagName('head')[0].appendChild(metaTag);

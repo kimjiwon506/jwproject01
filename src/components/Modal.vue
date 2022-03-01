779e4f2404a1e0a16cbebd59988dcc21febafc13
kimjiwon506 <kimjiwon506@gmail.com>
2022-03-01 18:26:58 +0900

    모달컴포넌트 기능만 구현해둠

---
+++ src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-show="open">
+    <slot />
+    <div>
+      <button @click="$emit('close', false)">확인</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  title: String,
+  subTitle: String,
+  open: Boolean,
+  },
+  data(){
+    return {
+  
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("close")
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/sample/ModalSample.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="open = true">클릭하면 모달오픈</button>
+    <Modal :open="open" @close="open = false">
+      slot으로 테스트
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from '../../components/Modal.vue'
+export default {
+  data(){
+    return {
+      open: false,
+    }
+  },
+  methods: {
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+
+</style>

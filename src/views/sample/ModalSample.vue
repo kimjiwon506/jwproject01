2e85a919582dd408765242eb091a1e3a9934fefc
kimjiwon506 <kimjiwon506@gmail.com>
2022-03-10 18:05:58 +0900

    토스트팝업

--- src/views/sample/ModalSample.vue
+++ src/views/sample/ModalSample.vue
@@ -1,65 +1,60 @@
 <template>
-  <div>
-    <div class="modalSampleBtn">
-      <button @click="open = true" >클릭하면 모달오픈</button>
-    </div>
-    <Modal :open="open" @close="open = false" okTitle="확인" cancleTitle="취소" >
-      <strong class="modalSampleTitle">확인용텍스트입니다확인용텍스트입니다확인다</strong>
-      <p class="modalSampleSubTitle">설명을 써주세요</p>
-    </Modal>
+  <div class="sample__formModal">
+    <!-- modal component -->
+    <!-- <section class="modalEvent__section">
+      <div class="modalSampleBtn">
+        <button @click="open = true">클릭하면 모달오픈</button>
+      </div>
+
+      <Modal
+        :open="open"
+        @close="open = false"
+        okTitle="확인"
+        cancleTitle="취소"
+      >
+        <strong class="modalSampleTitle"
+          >확인용텍스트입니다확인용텍스트입니다확인다</strong
+        >
+        <p class="modalSampleSubTitle">설명을 써주세요</p>
+      </Modal>
+    </section> -->
+    <!-- toast component -->
+    <section class="modalEvent__section">
+      <!-- 핸들클릭버튼 이벤트와 같이 사용해주기 -->
+      <button @click="handleClickButton">모달샘플</button>
+      <Toast :visible="visible">
+        <!-- toast이름을 붙였으므로 template안에 넣어서 사용해주기 -->
+        <template v-slot:toast>
+          <strong>토스트테스트입니다.</strong>
+        </template>
+      </Toast>
+    </section>
   </div>
 </template>
 
 <script>
-import Modal from '../../components/Modal.vue'
+import Modal from '../../components/Modal.vue';
+import Toast from '../../components/Toast.vue';
 export default {
-  data(){
+  data() {
     return {
-      open: false,
-    }
+      visible: false
+    };
   },
   methods: {
+    handleClickButton(){
+      if(true){
+        this.visible = true
+        setTimeout(()=>{this.visible = false},3000)
+      }
+    } 
   },
   components: {
-    Modal
-  }
-}
+    Modal,
+    Toast,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .modalSampleBtn {
-    position: fixed;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    button {
-      all: unset;
-      display: block;
-      padding: 8px 20px;
-      background: #cccccc;
-      border-radius: 6px;
-      color: #ffffff;
-    }
-  }
-  .modalSampleTitle {
-    display: block;
-    max-width: 320px;
-    width: 100%;
-    font-size: 16px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    margin-bottom: 10px;
-  }
-  .modalSampleSubTitle {
-    display: block;
-    max-width: 320px;
-    width: 100%;
-    font-size: 14px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    color: #212529;
-  }
 </style>
